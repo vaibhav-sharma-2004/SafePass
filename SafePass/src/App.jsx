870d7fe0ecd9088090fc55220cbf12e7aaa54c86
vaibhav-sharma-2004 <124458256+vaibhav-sharma-2004@users.noreply.github.com>
2024-10-20 18:12:23 +0530

    Add files via upload

---
+++ SafePass/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Manager from './components/Manager'
+import { Navbar } from './components/Navbar'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar />
+
+      <Manager />
+    </>
+  )
+}
+
+export default App
